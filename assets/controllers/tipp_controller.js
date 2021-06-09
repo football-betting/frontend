37312b479f4a53aafad98098226e8c38d77dfae6
Sebastian Felix Schwarz <s.schwarz@nexus-united.com>
2021-06-10 00:46:38 +0200

    kurwa

--- assets/controllers/tipp_controller.js
+++ assets/controllers/tipp_controller.js
@@ -1,14 +1,6 @@
 import { Controller } from 'stimulus';
 
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
- */
+
 export default class extends Controller {
     connect() {
         const scope = this;
@@ -20,14 +12,12 @@ export default class extends Controller {
                 scope.checkSubmit(formId.target.form,formId);
             }
         }
-
     }
     checkSubmit(form,formId){
         const url = "/test";
         let submit = false;
-        console.log(formId.srcElement.form[0].value);
         let obj = {
-            matchId: formId,
+            matchId: form.id,
             tipTeam1: null,
             tipTeam2: null
         }
@@ -39,15 +29,12 @@ export default class extends Controller {
             obj.tipTeam2 = formId.srcElement.form[1].value;
         }
         if(submit){
-            console.log("send items");
-            console.log(obj);
             fetch(url, {
                 method: "POST",
                 body: JSON.stringify(obj)
             }).then(res => {
                 console.log("Request complete! response:", res);
             });
-
         }
     }
 }

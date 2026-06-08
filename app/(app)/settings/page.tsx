5cd363a23af77fd2721c573ac7e2d25f28ab51b8
Rafal Wesolowski <info@styleax.de>
2026-06-08 22:22:20 +0200

    FE-086: highlight Settings (not Profile) in the bottom nav on the settings page

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -133,7 +133,7 @@ export default async function SettingsPage(): Promise<React.ReactElement> {
         </div>
       </main>
 
-      <BottomNav active="profile" />
+      <BottomNav active="settings" />
     </>
   );
 }
